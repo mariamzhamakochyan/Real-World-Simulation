ef2c244c9406e652d3de034341ee169c92879db4
mariamzhamakochyan <mariamzhamakochyan@gmail.com>
2023-02-09 12:22:51 +0400

    New Commit

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-from main.py import Universe
-u = Universe()
-input("Press enter to know more about the universe...")
-u.start()
+# from main import Universe
+
+
+# u = Universe()
+# input("Press enter to know more about the universe...")
+# u.start()
+# e = Earth("The Milky Way Galaxy", "Earth", "4,543E9", 510072000, 5, 7)
+# e.description()
